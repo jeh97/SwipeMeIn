ddacd3918bba7201cc17130ba15f18dcbfce1599
Jacob Hausmann <jacob@Jacobs-MacBook-Pro.local>
2019-07-08 10:22:06 -0500

    Initial commit

---
+++ SwipeMeIn/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeMeIn
+//
+//  Created by Jacob Hausmann on 7/8/19.
+//  Copyright © 2019 Jacob Hausmann. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
